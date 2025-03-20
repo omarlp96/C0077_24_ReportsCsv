c42666da12746e0d680467842e7389c0edc39cc3
Alejandro <Escobar>
2025-03-20 08:10:31 +0100

    ok

---
+++ ProjectFiles/NetSolution/ReadTxT.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.S7TiaProfinet;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+#endregion
+
+public class ReadTxT : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
